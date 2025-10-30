d1a7aefd579b335ebfc7de706364a7f778a1b8c6
MonteLogic <dchavours@gmail.com>
2025-10-30 15:36:37 -0500

    A lot less white run off

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default async function RootLayout({
 
   const AppShell = (
     <html lang="en" className="[color-scheme:dark] dark">
-      <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
+      <body className="bg-gray-1100 bg-[url('/grid.svg')]">
         <GlobalNav userData={userData} />
         <div className="lg:pl-64 xl:pl-72">
           <div className="space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
